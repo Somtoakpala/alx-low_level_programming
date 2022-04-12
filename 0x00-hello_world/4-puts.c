0b070d8015d7c4f8422efe9243aef9a1fb78005f
Somtoakpala <akpalasomto@gmail.com>
2022-04-11 23:27:22 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
     *Return -Always 0(Success)
    */
 
-int main()
+int main(void)
 {
-  char my_String = "\"Programming is like building a multilingual puzzle";
-     puts(my_String);
+puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
