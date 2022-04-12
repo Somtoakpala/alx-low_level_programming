83ffed2155874d2bdec4e6354ab69eaf3a70160b
Somtoakpala <akpalasomto@gmail.com>
2022-04-11 23:16:11 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+   /* 
+    *main - Entry point 
+    *
+    *Return -Always 0(Success)
+   */
+
+int main()
+{
+  string my_String = "\"Programming is like building a multilingual puzzle";
+     puts(my_String);
+return 0;
+}
