696dcf1b9ebd87bcc9675fbd3117f8ab2ebdaee2
Somtoakpala <akpalasomto@gmail.com>
2022-10-17 15:25:17 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+
+digit = 0;
+for (digit = 0; digit < 10; digit++)
+{
+printf(digit); }
+putchar('\n');
+return (0);
+}
