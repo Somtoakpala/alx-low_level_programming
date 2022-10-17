ebd3ff1dd21252af40cbc3112a2858193f8fd94d
Somtoakpala <akpalasomto@gmail.com>
2022-10-17 15:31:48 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha;
+
+alpha = '0';
+for (alpha = '0'; alpha < '10'; alpha++)
+{
+putchar(alpha);
+} }
+putchar('\n');
+return (0);
+}
