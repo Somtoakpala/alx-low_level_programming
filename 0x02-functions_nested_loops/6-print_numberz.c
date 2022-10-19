766b3586f13b6bf802a500f0e009916885ca81a7
Somtoakpala <akpalasomto@gmail.com>
2022-10-19 13:53:46 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alpha;
+
+alpha = 0;
+for (alpha = 0; alpha <= 9; alpha++)
+{
+putchar(alpha + '0'); }
+putchar('\n');
+return (0);
+}
